62fc7b9929f0ce55215ee518b028741d866dd35b
Laura LOspitao <lauralospitao@gmail.com>
2020-10-07 09:12:24 +0200

    #22 Visualize both available katas and lesson katas

--- src/Controller/LessonsEditorController.php
+++ src/Controller/LessonsEditorController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Kata;
+use App\Entity\Lesson;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,8 +16,6 @@ class LessonsEditorController extends AbstractController
     {
 
         $availableKatas = [];
-
-
         //All katas
         $katasArray = $this->getDoctrine()
             ->getRepository(Kata::class)
@@ -25,19 +24,33 @@ class LessonsEditorController extends AbstractController
         foreach($katasArray as $kata) {
         //Get kata title
             $title = $kata->getKataTitle();
+        //Get Kata uuid
             $kata_uuid = $kata->getUuid();
-
+        //Push title and uuid of kata into availableKatas
             array_push($availableKatas, [
                 'title' => $title,
                 'katasUuid' => $kata_uuid,
             ]);
-
         }
+
+        $lessonKatasArray = [];
+        //get lesson through uuid
+        $lesson = $this->getDoctrine()
+            ->getRepository(Lesson::class)
+            ->findOneBy(['uuid' => $uuid]);
+        $lessonKatas = $lesson->getKata();
+        foreach($lessonKatas as $lessonKata) {
+            $title = $lessonKata->getKataTitle();
+            array_push($lessonKatasArray, ['title' => $title]);
+        }
+
+
         return $this->render('lessons_editor/index.html.twig', [
             'controller_name' => 'LessonsEditorController',
             'lesson_uuid' => $uuid,
             'availableKatas'=> $availableKatas,
             'kata_uuid' => $kata_uuid,
+            'lessonKatasArray'=> $lessonKatasArray,
         ]);
     }
 }
