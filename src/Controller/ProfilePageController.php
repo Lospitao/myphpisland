ba443e9d59f7dd87b060ca6a64b8d6cf5069f095
Laura LOspitao <lauralospitao@gmail.com>
2020-10-13 17:05:20 +0200

    #24 Create a password block

--- src/Controller/ProfilePageController.php
+++ src/Controller/ProfilePageController.php
@@ -3,22 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\User;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ProfilePageController extends AbstractController
 {
     /**
-     * @Route("/profile_page/", name="profile_page")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @Route("/profile_page/{username}", name="profile_page")
      */
-    public function index()
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder, $username)
     {
         return $this->render('profile_page/index.html.twig', [
             'controller_name' => 'ProfilePageController',
         ]);
     }
 
-    public function updatePassword() {
 
-    }
 }
