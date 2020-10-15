c8ddf438cad43bdde74cd1105e65e43a7a10bbc6
Laura LOspitao <lauralospitao@gmail.com>
2020-10-15 16:47:29 +0200

    #27 Create Ajax Call to manage Password Updating
    So far, ajax call is working. Pending work on handling passwords comparison in controller UpdatePasswordController.

---
+++ public/js/update-password.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    let element = document.getElementById("username");
+    let username = element.getAttribute("class")
+
+    let updateLessonWebService = 'https://localhost:8000/api/v1/users/' + username;
+
+
+
+    $('#update_password').click(function() {
+        let current_password = $('#current_password').val();
+        let new_password = $('#new_password').val();
+        let repeat_password = $('#repeat_password').val();
+        $.ajax({
+            url : updateLessonWebService,
+            data : {
+                'username' : username,
+                'current_password' : current_password,
+                'new_password' : new_password,
+                'repeat_password' : repeat_password,
+            },
+            type : 'POST',
+            dataType : 'json',
+            success: function (data) {
+                console.log('Submission was successful.');
+                console.log(data)
+            },
+            error: function (data) {
+                console.log('An error occurred.');
+                console.log(data);
+            },
+        })
+    });
+});
--- src/Controller/api/v1/users/UpdatePasswordController.php
+++ src/Controller/api/v1/users/UpdatePasswordController.php
@@ -1,43 +1,46 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\api\v1\users;
 
 use App\Entity\User;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-
-class ProfilePageController extends AbstractController
-{
+Class UpdatePasswordController extends AbstractController {
     /**
-     * @Route("/profile_page/{username}", name="profile_page")
+     * @Route("api/v1/users/{username}", name="UpdatePasswordController")
+     * @param Request $request
+     * @param $username
+     * @param $passwordEncoder
+     * @return JsonResponse
      */
-    public function Index($username)
-    {
+    function UpdatePassword(Request $request, $username, UserPasswordEncoderInterface $passwordEncoder) {
         //Get user through usernames
         $user = $this->getDoctrine()
             ->getRepository(User::class)
             ->findOneBy(['username' => $username]);
-        //If form is submitted
-/*
+        $name = $user->getName();
         //Get password registered in DataBase
         $registered_password = $user->getPassword();
         //Get password input values
         $current_password = $request->request->get('current_password');
         $new_password = $request->request->get('new_password');
         $repeat_password = $request->request->get('repeat_password');
+        /*
         //Encode password specified in current password field
         $encoded_current_password = $passwordEncoder->encodePassword($user, $current_password);
         //Encode password specified in new password field
         $encoded_new_password = $passwordEncoder->encodePassword($user, $new_password);
-        //If current_password exists in database
-        If ($registered_password == $encoded_current_password) {
-            //If new password is != current password
-            if ($encoded_current_password != $encoded_new_password) {
+        */
+        //If current_password is registered password
+        If ($current_password->passwordEncoder->isPasswordValid($user, $registered_password)) {
                 //If repeat password == new password
                 if ($new_password == $repeat_password) {
+                    //Encode new password
+                    $encoded_new_password = $passwordEncoder->encodePassword($user, $new_password);
                     //Set password to new password
                     $user->setPassword($encoded_new_password);
                     //Set success message
@@ -48,21 +51,13 @@ class ProfilePageController extends AbstractController
                     $entity_manager->flush();
                 }
                 else $result_message = "Repite la misma contraseña";
-            }
-            else $result_message = "Usa una contraseña nueva";
         }
         else $result_message = "La contraseña actual especificada no existe";
 
-        function phpAlert($result_message) {
-            echo '<script type="text/javascript">alert("' . $result_message . '")</script>';
-        }
-
-*/
-
-        return $this->render('profile_page/index.html.twig', [
-            'controller_name' => 'ProfilePageController',
+        $response = new JsonResponse([
+            'result_message' => $result_message,
         ]);
+
+        return $response;
     }
-
-
 }
