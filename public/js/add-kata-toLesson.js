8978e24dab05c8da42cdcc27dbdd27f75e6b8bdd
Laura LOspitao <lauralospitao@gmail.com>
2020-10-23 13:35:02 +0200

    #33 Refactor to encapsulate addKataEvent and removeKataEvent functions
    PROBLEM: Al igual que antes del a encapsulación, puedo pasar la kata a lesson katas y devolverla a available katas pero tras devolverla a available katas ya no puedo pasarla por segunda vez a lesson katas.
    
    Por si no te orientas, el success que coge en primera instancia cuando paso la kata a la lesson por primera vez es el de la línea 61, pero el success que toma en cuenta cuando paso la kata por segunda vez es el de la línea 30 y este no tiene asociado el evento de click (que podría asignarlo pero sería un bucle infinito)

--- public/js/add-kata-toLesson.js
+++ public/js/add-kata-toLesson.js
@@ -7,7 +7,40 @@ $(document).ready(function() {
 
     let updateLessonWebService = 'https://localhost:8000/api/v1/lessons/' + uuid + '/katas' ;
 
+    function removeKataEvent() {
 
+        //IT IS NECESSARY TO ASSING THE CLICK EVENT AS DOCUMENT IS ALREADY READY WHEN WE ADD THE NEW EVENT TO THE DOM AND OTHERWISE THE EVENT WOULD NOT TRIGGER
+        $('.removeKataFromLesson').click(function() {
+
+            let kataUuid = $(this).attr("data-uuid");
+            let kataToBeRemovedFromLesson = $(this).attr("data-title");
+            let removingKata = $(this).parent().parent();
+
+
+            $.ajax({
+
+                url : updateLessonWebService,
+                data : {
+                    'kataUuid' : kataUuid,
+                    'kataToBeRemovedFromLesson' : kataToBeRemovedFromLesson,
+
+                },
+                type : 'POST',
+                dataType : 'json',
+                success: function (data) {
+                    console.log('Submission was successful.');
+                    let kataToRemoveFromLesson =`<li>${kataToBeRemovedFromLesson}<a><i class="tiny material-icons addAvailableKataToLesson" data-title="${kataToBeRemovedFromLesson}" data-uuid="${kataUuid}">add_circle</i></a><br></li>`;
+                    $( ".availableKatasList" ).append(kataToRemoveFromLesson);
+                    $(removingKata).remove();
+                },
+                error: function (data) {
+                    console.log('An error occurred.');
+                    console.log(data['kata_uuid']);
+
+                },
+            })
+        })
+    }
 
     $('.addAvailableKataToLesson').click(function() {
 
@@ -30,44 +63,7 @@ $(document).ready(function() {
                 let newLessonKata = `<li>${kataToBeAdded}<a href="#"><i class="tiny material-icons removeKataFromLesson" data-title="${kataToBeAdded}" data-uuid="${kataUuid}">clear</i></a></li><br>`;
                 $( ".lessonKatas" ).append(newLessonKata);
                 $(removingKata).remove();
-
-                //IT IS NECESSARY TO ASSING THE CLICK EVENT AS DOCUMENT IS ALREADY READY WHEN WE ADD THE NEW EVENT TO THE DOM AND OTHERWISE THE EVENT WOULD NOT TRIGGER
-                $('.removeKataFromLesson').click(function() {
-
-                    let title = document.getElementById("title");
-                    let uuid = title.getAttribute("data-uuid");
-
-                    let updateLessonWebService = 'https://localhost:8000/api/v1/lessons/' + uuid + '/katas' ;
-
-                    let kataUuid = $(this).attr("data-uuid");
-                    let kataToBeRemovedFromLesson = $(this).attr("data-title");
-                    let removingKata = $(this).parent().parent();
-
-
-                    $.ajax({
-
-                        url : updateLessonWebService,
-                        data : {
-                            'kataUuid' : kataUuid,
-                            'kataToBeRemovedFromLesson' : kataToBeRemovedFromLesson,
-
-                        },
-                        type : 'POST',
-                        dataType : 'json',
-                        success: function (data) {
-                            console.log('Submission was successful.');
-                            let kataToRemoveFromLesson =`<li>${kataToBeRemovedFromLesson}<a><i class="tiny material-icons addAvailableKataToLesson" data-title="${kataToBeRemovedFromLesson}" data-uuid="${kataUuid}">add_circle</i></a><br></li>`;
-                            $( ".availableKatasList" ).append(kataToRemoveFromLesson);
-                            $(removingKata).remove();
-
-                        },
-                        error: function (data) {
-                            console.log('An error occurred.');
-                            console.log(data['kata_uuid']);
-
-                        },
-                    })
-                })
+                $(".removeKataFromLesson").click(removeKataEvent());
             },
             error: function (data) {
                 console.log('An error occurred.');
--- public/js/remove-kata-fromLesson.js
+++ public/js/remove-kata-fromLesson.js
@@ -7,6 +7,38 @@ $(document).ready(function() {
     let uuid = title.getAttribute("data-uuid");
     let updateLessonWebService = 'https://localhost:8000/api/v1/lessons/' + uuid + '/katas' ;
 
+    function addKataEvent() {
+        //IT IS NECESSARY TO ASSING THE CLICK EVENT AS DOCUMENT IS ALREADY READY WHEN WE ADD THE NEW EVENT TO THE DOM AND OTHERWISE THE EVENT WOULD NOT TRIGGER
+        $('.addAvailableKataToLesson').click(function() {
+
+            let kataUuid = $(this).attr("data-uuid");
+            let kataToBeAdded = $(this).attr("data-title");
+            let removingKata = $(this).parent().parent();
+
+
+
+            $.ajax({
+
+                url : updateLessonWebService,
+                data : {
+                    'kataUuid' : kataUuid,
+                    'kataToBeAdded' : kataToBeAdded,
+                },
+                type : 'POST',
+                dataType : 'json',
+                success: function (data) {
+                    console.log('Submission was successful.');
+                    let newLessonKata = `<li>${kataToBeAdded}<a href="#"><i class="tiny material-icons removeKataFromLesson" data-title="${kataToBeAdded}" data-uuid="${kataUuid}">clear</i></a></li><br>`;
+                    $( ".lessonKatas" ).append(newLessonKata);
+                    $(removingKata).remove();
+                },
+                error: function (data) {
+                    console.log('An error occurred.');
+
+                },
+            })
+        });
+    }
     $('.removeKataFromLesson').click(function() {
 
         let kataUuid = $(this).attr("data-uuid");
@@ -29,44 +61,7 @@ $(document).ready(function() {
                 let kataToRemoveFromLesson =`<li>${kataToBeRemovedFromLesson}<a><i class="tiny material-icons addAvailableKataToLesson" data-title="${kataToBeRemovedFromLesson}" data-uuid="${kataUuid}">add_circle</i></a><br></li>`;
                 $( ".availableKatasList" ).append(kataToRemoveFromLesson);
                 $(removingKata).remove();
-
-                //IT IS NECESSARY TO ASSING THE CLICK EVENT AS DOCUMENT IS ALREADY READY WHEN WE ADD THE NEW EVENT TO THE DOM AND OTHERWISE THE EVENT WOULD NOT TRIGGER
-                $('.addAvailableKataToLesson').click(function() {
-
-                    //Select element with id title (lesson title textarea)
-                    let title = document.getElementById("title");
-                    //get lesson uuid
-                    let uuid = title.getAttribute("data-uuid");
-
-                    let updateLessonWebService = 'https://localhost:8000/api/v1/lessons/' + uuid + '/katas' ;
-                    let kataUuid = $(this).attr("data-uuid");
-                    let kataToBeAdded = $(this).attr("data-title");
-                    let removingKata = $(this).parent().parent();
-
-
-
-                    $.ajax({
-
-                        url : updateLessonWebService,
-                        data : {
-                            'kataUuid' : kataUuid,
-                            'kataToBeAdded' : kataToBeAdded,
-                        },
-                        type : 'POST',
-                        dataType : 'json',
-                        success: function (data) {
-                            console.log('Submission was successful.');
-                            let newLessonKata = `<li>${kataToBeAdded}<a href="#"><i class="tiny material-icons removeKataFromLesson data-title="${kataToBeAdded}" data-uuid="${kataUuid}"">clear</i></a></li><br>`;
-                            $( ".lessonKatas" ).append(newLessonKata);
-                            $(removingKata).remove();
-                        },
-                        error: function (data) {
-                            console.log('An error occurred.');
-
-                        },
-                    })
-                });
-
+                $(".addAvailableKataToLesson").click(addKataEvent());
             },
             error: function (data) {
                 console.log('An error occurred.');
