d0001fe08cd8ed4b07892f8098a6e55e32359b0a
Laura LOspitao <lauralospitao@gmail.com>
2020-11-03 10:41:15 +0100

    #37 Edit Lesson view page
    When in a lesson view page, we can see the available katas as well as the lesson katas and all of them are clickable and redirect to the relevant kata view page

--- src/Controller/LessonsController.php
+++ src/Controller/LessonsController.php
@@ -13,7 +13,8 @@ class LessonsController extends AbstractController
     /**
      * @Route("/lessons/create", name="lessons_create" )
      */
-    public function lessonscreate() {
+    public function lessonscreate()
+    {
         $lesson = new Lesson();
         $uuid = Uuid::v4();
         $lesson->setUuid($uuid);
@@ -31,29 +32,63 @@ class LessonsController extends AbstractController
      */
     public function index($uuid)
     {
-        $availableKatas = array();
-        //Todas las katas
-        $katasArray = $this->getDoctrine()
-            ->getRepository(Kata::class)
-            ->findAll();
-        //Iteración dentro del arry de katas
-        foreach($katasArray as $kata) {
-            //Obtener el título cada
-                $title = $kata->getKataTitle();
-                array_push($availableKatas, $title);
-
-        }
+        //Load Katas that are already in the lesson as index is loaded
+        //Create array where each kata title and uuid will be stored
+        $lessonKatasArray = [];
+        $availableKatas = [];
+        //get lesson through uuid
         $lesson = $this->getDoctrine()
             ->getRepository(Lesson::class)
             ->findOneBy(['uuid' => $uuid]);
-        $lessonTitle= $lesson->getTitle();
+        //get lesson title
+        $lessonTitle = $lesson->getTitle();
+        //get kata collection related to lesson
+        $lessonKatas = $lesson->getKata();
+
+        //iteration inside lessonKatas
+        foreach($lessonKatas as $lessonKata) {
+            //get kata title
+            $lessonKatasTitle = $lessonKata->getKataTitle();
+            //get kata uuid
+            $lessonKatasUuid = $lessonKata->getUuid();
+            //Push both into the array previously created
+            $lessonKatasArray[$lessonKatasUuid]= [
+                'title' => $lessonKatasTitle,
+                'uuid' => $lessonKatasUuid,
+            ];
+        }
+
+        //Load Available katas as index is loaded
+        $availableKatas = [];
+        //All katas
+        $katasArray = $this->getDoctrine()
+            ->getRepository(Kata::class)
+            ->findAll();
+        //Iteration inside katas array
+        foreach($katasArray as $kata) {
+            //Get kata title
+            $title = $kata->getKataTitle();
+            //Get Kata uuid
+            $kata_uuid = $kata->getUuid();
+            //If Kata is inside of lesson katas, remove it from availableKatas
+            if(!array_key_exists($kata_uuid, $lessonKatasArray)) {
+                //Push title and uuid of kata into availableKatas
+                $availableKatas[$kata_uuid] = [
+                    'title' => $title,
+                    'uuid' => $kata_uuid,
+                ];
+            }
+        }
+
 
         return $this->render('lessons/index.html.twig', [
-            'controller_name' => 'LessonsController',
+            'controller_name' => 'LessonsEditorController',
+            'lesson_uuid' => $uuid,
             'availableKatas'=> $availableKatas,
-            'uuid' => $uuid,
+            'kata_uuid' => $kata_uuid,
+            'lessonKatasArray'=> $lessonKatasArray,
+            'lessonKatas' => $lessonKatas,
             'lessonTitle' => $lessonTitle,
-
         ]);
     }
 }
