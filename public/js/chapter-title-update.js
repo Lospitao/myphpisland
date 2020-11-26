01fd620b2d19ff893e603068685d71f4524655a8
Laura LOspitao <lauralospitao@gmail.com>
2020-11-26 17:00:04 +0100

    #53 Create Chapter Title Update api

---
+++ public/js/chapter-title-update.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function() {
+
+    let chapterTitle = document.getElementById("chapter-title");
+    let chapterUuid = chapterTitle.getAttribute("data-uuid")
+
+    let updateLessonWebService = 'https://localhost:8000/api/v1/chapters/' + chapterUuid + '/title';
+
+
+
+    $('#chapter-title').focusout(function() {
+        let chapter_title_text = $('#chapter-title').val();
+        $.ajax({
+            url : updateLessonWebService,
+            data : {
+                'chapter_title' : chapter_title_text,
+            },
+            type : 'PATCH',
+            dataType : 'json',
+            success: function (data) {
+                console.log('Submission was successful.');
+                console.log(data)
+
+            },
+            error: function (data) {
+                console.log('An error occurred.');
+                console.log(data['stageUuidDevuelto']);
+            },
+        })
+    });
+});
