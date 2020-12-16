6067e00dfd7323b3d747bd3d6c1eaebf02880c36
Jorge Sánchez <jorge.sanchez@digimobil.es>
2020-12-16 08:27:38 +0100

    Add TestExecutor with a "static test file content" to test the source code.

--- src/Controller/api/v1/test_executions/TestExecutionsController.php
+++ src/Controller/api/v1/test_executions/TestExecutionsController.php
@@ -4,7 +4,10 @@ namespace App\Controller\api\v1\test_executions;
 
 
 use App\Entity\Kata;
+use Exception;
+use PhpunitExecutionFromPhp\TestExecutionStatus;
 use PhpunitExecutionFromPhp\TestExecutor;
+use PhpunitExecutionFromPhp\TestResult;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,37 +20,128 @@ Class TestExecutionsController extends AbstractController {
      * @return JsonResponse
      */
     function TestExecutions (Request $request, $uuid) {
-        //Select kata from uuid
-        $kata = $this->getDoctrine()
-            ->getRepository(Kata::class)
-            ->findOneBy(['uuid' => $uuid]);
+        try {
 
-        $editorCode=$request->request->get('code');
-        $sampleTest = $kata->getTestCode();
+            //Select kata from uuid
+            $kata = $this->getDoctrine()
+                ->getRepository(Kata::class)
+                ->findOneBy(['uuid' => $uuid]);
 
-        $projectRoot = $this->get('kernel')->getProjectDir();
+            $editorCode = $request->request->get('code');
 
-        $temporaryFilesPath = $projectRoot . DIRECTORY_SEPARATOR . 'tmp' ;
-        if ( ! is_dir($temporaryFilesPath) && ! mkdir($temporaryFilesPath)) {
-            throw new \RuntimeException(sprintf('Directory "%s" was not created', $temporaryFilesPath));
+            $projectRoot = $this->getParameter('kernel.project_dir');
+
+            $temporaryFilesPath = $projectRoot . DIRECTORY_SEPARATOR . 'tmp' ;
+            if ( ! is_dir($temporaryFilesPath) && (! mkdir($temporaryFilesPath))) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $temporaryFilesPath));
+            }
+
+            // Execute test
+            $phpunitShellPath = $projectRoot . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'phpunit';
+            $phpunitBootstrapShellPath = $projectRoot . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+            $testExecutor = new TestExecutor($temporaryFilesPath .  DIRECTORY_SEPARATOR, $phpunitShellPath, $phpunitBootstrapShellPath);
+
+            // Create kata test file from string
+
+            $kataTestCode = <<<'EOD'
+<?php 
+namespace KataTestExecutions;
+
+use PHPUnit\Framework\TestCase;
+
+
+class AlwaysPassedTest extends TestCase
+{
+    public function testAlwaysPassed()
+    {
+        $alwaysPassedSourceCode = new AlwaysPassedSourceCode();
+        $semanticNames = $alwaysPassedSourceCode->getSemanticNames();
+        $areNamesValid = ($semanticNames['Nombre de usuario'] === 'username')
+            && ($semanticNames['Correo electrónico'] === 'email')
+            && ($semanticNames['contraseña'] === 'password');
+
+        $this->assertTrue($areNamesValid);
+    }
+}
+
+EOD;
+
+            $kataTestExecutionsPath = $projectRoot . DIRECTORY_SEPARATOR . 'tmp' .  DIRECTORY_SEPARATOR ;
+            $kataTestPath = $kataTestExecutionsPath . 'AlwaysPassedTest.php';
+            $isSaved = file_put_contents($kataTestPath, $kataTestCode);
+            if ($isSaved === false) {
+                throw new Exception('Kata test file has not could to be created.');
+            }
+
+            // Execute the test
+            $testResult = $testExecutor->execute($kataTestPath, $editorCode);
+
+            if ($this->hasAnErrorHadDuringTheTestExecution($testResult)) {
+                throw new Exception('Se ha producido un error al ejecutar el test.');
+            }
+
+            if ($this->hasTestFailed($testResult)) {
+                $response = $this->getFailResponse();
+
+                return $response;
+            }
+
+            $response = $this->getSuccessResponse();
+
+            return $response;
+        } catch (Exception $exception) {
+            $response = new JsonResponse([
+                'error' => 'Ha petao!' . $exception->getMessage()
+            ], 409);
+
+            return $response;
         }
 
-        // Execute test
-        $phpunitShellPath = $projectRoot . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'phpunit';
-        $phpunitBootstrapShellPath = $projectRoot . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-        $testExecutor = new TestExecutor($temporaryFilesPath, $phpunitShellPath, $phpunitBootstrapShellPath);
+    }
 
-        if ($editorCode == $sampleTest) {
-            $isTestPassed = true;
-            $title = "Passed test";
-            $message= "You are right";
-        }
+    /**
+     * @param TestResult $testResult
+     * @return bool
+     */
+    private function hasAnErrorHadDuringTheTestExecution(TestResult $testResult): bool
+    {
+        return $testResult->getTestExecutionStatusUuid() === TestExecutionStatus::UUID_ERROR;
+    }
 
-        else {
-            $isTestPassed = false;
-            $title = "Test Failed";
-            $message= "Keep trying";
-        }
+    /**
+     * @param TestResult $testResult
+     * @return bool
+     */
+    private function hasTestFailed(TestResult $testResult): bool
+    {
+        return $testResult->getTestExecutionStatusUuid() === TestExecutionStatus::UUID_FAIL;
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    private function getFailResponse(): JsonResponse
+    {
+        $isTestPassed = false;
+        $title = "Test Failed";
+        $message = "Keep trying";
+        $response = new JsonResponse([
+            'isTestPassed' => $isTestPassed,
+            'title' => $title,
+            'message' => $message,
+
+        ]);
+        return $response;
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    private function getSuccessResponse(): JsonResponse
+    {
+        $isTestPassed = true;
+        $title = "Passed test";
+        $message = "You are right";
 
         $response = new JsonResponse([
             'isTestPassed' => $isTestPassed,
@@ -55,8 +149,6 @@ Class TestExecutionsController extends AbstractController {
             'message' => $message,
 
         ]);
-
         return $response;
-
     }
 }
