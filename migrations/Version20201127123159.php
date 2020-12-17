9be2d21ec1f0f9587ad83ae84d678f1a6c4ffc67
Jorge Sánchez <jorge.sanchez@digimobil.es>
2020-12-17 19:30:39 +0100

    Fix sql migration

--- migrations/Version20201127123159.php
+++ migrations/Version20201127123159.php
@@ -20,7 +20,9 @@ final class Version20201127123159 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE chapter_element_type RENAME COLUMN chapter_element to name');
+//        $this->addSql('ALTER TABLE chapter_element_type RENAME COLUMN chapter_element to name');
+        $this->addSql('ALTER TABLE `chapter_element_type` CHANGE COLUMN `chapter_element` `name` VARCHAR(255) NULL DEFAULT NULL COLLATE \'utf8mb4_unicode_ci\' AFTER `id`;
+');
 
     }
 
