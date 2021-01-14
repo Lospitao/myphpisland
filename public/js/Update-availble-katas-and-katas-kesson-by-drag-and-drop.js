18ced294e435e9938d2cb201aa6dd527f4ad918d
Laura LOspitao <lauralospitao@gmail.com>
2021-01-14 14:18:50 +0100

    #69 Make a web service to add katas to a lesson through drag and drop

--- public/js/Update-availble-katas-and-katas-kesson-by-drag-and-drop.js
+++ public/js/Update-availble-katas-and-katas-kesson-by-drag-and-drop.js
@@ -1,11 +1,70 @@
+//Define function to update position
 function updatePositionKatasList() {
-
-
     $( ".lessonKataElement" ).each(function( index ) {
-        return index;
+        console.log( index + ": " + $( this ).text() );
     });
 }
 
+function addKataToLessonByDragAndDrop (kataToBeAddedTitle, kataToBeAddedUuid, availableKataSortable, sortablesender) {
+    //Select element with id title (lesson title textarea)
+    let title = document.getElementById("title");
+    //get lesson uuid
+    let uuid = title.getAttribute("data-uuid");
+    let updateLessonWebService = 'https://localhost:8000/api/v1/lessons/dragdrop/' + uuid + '/katas' ;
+
+    $.ajax({
+        url : updateLessonWebService,
+        data : {
+            'kataToBeAddedUuid' : kataToBeAddedUuid,
+            'kataToBeAddedTitle' : kataToBeAddedTitle,
+        },
+        type : 'POST',
+        dataType : 'json',
+        success: function (data) {
+            console.log('Submission was successful.');
+
+            if (sortablesender) {
+                //define newLessonKata
+                let newLessonKata = `<li class="ui-state-highlight lessonKataElement ui-sortable-handle" data-title="${kataToBeAddedTitle}" data-uuid="${kataToBeAddedUuid}">${kataToBeAddedTitle}<a href="#"><i class="tiny material-icons lessonKata" data-title="${kataToBeAddedTitle}" data-uuid="${kataToBeAddedUuid}">clear</i></a></li>`;
+                availableKataSortable.replaceWith(newLessonKata);
+
+            }
+        },
+        error: function (data) {
+            console.log('An error occurred.');
+        },
+    })
+}
+function removeKataFromLessonByDragAndDrop (kataToBeReturnedToAvailableTitle, kataToBeReturnedToAvailableUuid, lessonKataSortable, sortablesender) {
+    //Select element with id title (lesson title textarea)
+    let title= document.getElementById("title");
+    //get lesson uuid
+    let uuid = title.getAttribute("data-uuid");
+    let updateLessonWebService = 'https://localhost:8000/api/v1/lessons/dragdrop/' + uuid + '/katas/' + kataToBeReturnedToAvailableUuid ;
+
+
+    $.ajax({
+
+        url : updateLessonWebService,
+        data : {
+            'kataToBeRemovedUuid' : kataToBeReturnedToAvailableUuid,
+            'kataTitleToBeRemovedFromLesson' : kataToBeReturnedToAvailableTitle,
+        },
+        type : 'DELETE',
+        dataType : 'json',
+        success: function (data) {
+            console.log('Submission was successful.');
+            if (sortablesender) {
+                //define kataToBeReturned
+                let kataToBeReturned = `<li class="ui-state-highlight availableKataElement ui-sortable-handle" data-title="${kataToBeReturnedToAvailableTitle}" data-uuid="${kataToBeReturnedToAvailableUuid}">${kataToBeReturnedToAvailableTitle}<a href="#"><i class="tiny material-icons lessonKata" data-title="${kataToBeReturnedToAvailableTitle}" data-uuid="${kataToBeReturnedToAvailableUuid}">add_circle</i></a></li>`;
+                lessonKataSortable.replaceWith(kataToBeReturned);
+            }
+        },
+        error: function (data) {
+            console.log('An error occurred.');
+        },
+    })
+}
 function makeKataListSortable() {
 
     $( "#availableKatasList, #lessonKatasList").sortable({
@@ -13,33 +72,26 @@ function makeKataListSortable() {
         receive: function (event, ui) {
             //define element to be moved class
             let elementClass = (ui.item.context).getAttribute("class");
-            console.log(elementClass);
-            //If the moved element is an available Kata
-            if (elementClass.includes("availableKataElement")) {
-                //define kataToBeAddedTitle and kataToBeAddedTitle
-                let kataToBeAddedTitle = (ui.item.context).getAttribute("data-title");
-                let kataToBeAddedUuid = (ui.item.context).getAttribute("data-uuid");
-                //define newLessonKata
-                let newLessonKata = `<li class="ui-state-highlight lessonKataElement ui-sortable-handle" data-title="${kataToBeAddedTitle}" data-uuid="${kataToBeAddedUuid}">${kataToBeAddedTitle}<a href="#"><i class="tiny material-icons lessonKata" data-title="${kataToBeAddedTitle}" data-uuid="${kataToBeAddedUuid}">clear</i></a></li>`;
-                //if an element was moved from one sortable list to a different one
-                if (ui.sender) {
-                    let availableKataSortable= ui.item ;
-                    availableKataSortable.replaceWith(newLessonKata);
-                }
-            }
-            //else (if the moved element is a lesson Kata)
+            //If the moved element is an lesson Kata
+            if (elementClass.includes("lessonKataElement")) {
                 //define kataToBeReturnedToAvailableTitle and kataToBeReturnedToAvailableUuid
                 let kataToBeReturnedToAvailableTitle = (ui.item.context).getAttribute("data-title");
                 let kataToBeReturnedToAvailableUuid = (ui.item.context).getAttribute("data-uuid");
-                //define kataToBeReturned
-                let kataToBeReturned = `<li class="ui-state-highlight lessonKataElement ui-sortable-handle" data-title="${kataToBeReturnedToAvailableTitle}" data-uuid="${kataToBeReturnedToAvailableUuid}">${kataToBeReturnedToAvailableTitle}<a href="#"><i class="tiny material-icons lessonKata" data-title="${kataToBeReturnedToAvailableTitle}" data-uuid="${kataToBeReturnedToAvailableUuid}">add_circle</i></a></li>`;
-                //if an element was moved from one sortable list to a different one
-                if (ui.sender) {
-                    let lessonKataSortable= ui.item ;
-                    lessonKataSortable.replaceWith(kataToBeReturned);
-                }
+                let lessonKataSortable = ui.item;
+                let sortablesender = ui.sender;
+                removeKataFromLessonByDragAndDrop (kataToBeReturnedToAvailableTitle, kataToBeReturnedToAvailableUuid, lessonKataSortable, sortablesender);
+            }
+            //else (if the moved element is an available kata)
+            else {
+                //define kataToBeAddedTitle and kataToBeAddedTitle
+                let kataToBeAddedTitle = (ui.item.context).getAttribute("data-title");
+                let kataToBeAddedUuid = (ui.item.context).getAttribute("data-uuid");
+                let availableKataSortable = ui.item;
+                let sortablesender = ui.sender;
+                addKataToLessonByDragAndDrop (kataToBeAddedTitle, kataToBeAddedUuid, availableKataSortable, sortablesender);
+            }
         },
-        update: function (event, ui) {
+        out: function (event, ui) {
             updatePositionKatasList();
         },
         revert:true,
--- src/Controller/api/v1/lessons/dragdrop/AddKataToLessonByDragAndDropController.php
+++ src/Controller/api/v1/lessons/dragdrop/AddKataToLessonByDragAndDropController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\api\v1\lessons;
+namespace App\Controller\api\v1\lessons\dragdrop;
 
 use App\Entity\Kata;
 use App\Entity\Lesson;
@@ -10,38 +10,35 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-Class AddKataToLessonController extends AbstractController
+class AddKataToLessonByDragAndDropController extends AbstractController
 {
     /**
-     * @Route("api/v1/lessons/{uuid}/katas", name="AddKataToLessonController")
+     * @Route("api/v1/lessons/dragdrop/{uuid}/katas", name="AddKataToLessonByDragAndDropController")
      * @param $uuid,
      * @return JsonResponse
      */
-    function AddKataToLessonController (Request $request, $uuid)
+    function AddKataToLessonByDragAndDropController (Request $request, $uuid)
     {
         //get kata uuid
-        $kataUuid =$request->request->get('kataToBeAddedUuid');
-        // get position
-        $newKataPosition = $request->request->get('positionOfNewLessonKata');
+        $kataUuid = $request->request->get('kataToBeAddedUuid');
         //get kata to be added
-        $kata= $this->getDoctrine()
+        $kata = $this->getDoctrine()
             ->getRepository(Kata::class)
             ->findOneBy(['uuid' => $kataUuid]);
-
         //get lesson to be updated
         $lesson = $this->getDoctrine()
             ->getRepository(Lesson::class)
             ->findOneBy(['uuid' => $uuid]);
-
         //check the lesson exists
         if ($uuid) {
             //check if kata exists
             if ($kata) {
-                //Add kata to lesson in relational tabqle lessonKatas
-                $newLessonKata =  new LessonKatas();
+                //Add kata to lesson in relational table lessonKatas
+                $newLessonKata = new LessonKatas();
                 $newLessonKata->setKata($kata);
                 $newLessonKata->setLesson($lesson);
-                $newLessonKata->setPosition($newKataPosition);
+                $position = 222;
+                $newLessonKata->setPosition($position);
                 //persist kata to lesson (lesson-kata table in DB)
                 $entity_manager = $this->getDoctrine()->getManager();
                 $entity_manager->persist($newLessonKata);
@@ -50,8 +47,7 @@ Class AddKataToLessonController extends AbstractController
                 $response = new JsonResponse();
                 $response->setStatusCode(JsonResponse::HTTP_NO_CONTENT);
                 return $response;
-            }
-            else {
+            } else {
                 $response = new JsonResponse();
                 $response->setStatusCode(JsonResponse::HTTP_BAD_REQUEST);
                 return $response;
