10ef13ef6d92554c83ed682faab37a07fdeac4bc
Laura LOspitao <lauralospitao@gmail.com>
2021-05-04 11:58:21 +0200

    #178 Stages > "Next" Button appears only when dialogs have finished playing
    When user first arrives to stage, ambient sound starts playing straight away and dialog starts once user has clicked anywhere on the image and once dialog has finished playing, next button will appear

--- public/js/stageView/stage-ambient-dialog-controls.js
+++ public/js/stageView/stage-ambient-dialog-controls.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
     let title = document.getElementById("stage-view");
     let stageUuid = title.getAttribute("data-uuid");
+
     //Ambient sound starts playing once the stage is loaded
     var sound = new Howl({
         src: ['/resources/stages/' + stageUuid + '/ambient.mp3'],
@@ -10,11 +11,17 @@ $(document).ready(function() {
         mute: false,
     });
     sound.play();
+
     // Dialog starts when user clicks anywhere on the image
     $('#background_image').click(function () {
         var dialog = new Howl ({
             src:['/resources/stages/' + stageUuid + '/dialog.mp3'],
             volume: 0.5,
+            onend: function () {
+                let nextPageRoute = 'https://localhost:8000/next/page'
+                let nextButton = `<a class="waves-effect waves-light btn-large float-right" href=${nextPageRoute}>Siguiente</a>`
+                $( ".next_button_section" ).append(nextButton);
+            }
         });
         dialog.play();
     })
